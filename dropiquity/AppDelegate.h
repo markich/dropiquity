05a9b238a19a6f8a852462446d079c934ea0b047
Marcos Jesus Vivar <mvivar@devspark.com>
2014-09-03 11:22:27 -0300

    Initial Commit

---
+++ dropiquity/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  dropiquity
+//
+//  Created by Marcos Jesús Vivar on 9/3/14.
+//  Copyright (c) 2014 DevSpark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ dropiquity/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  dropiquity
+//
+//  Created by Marcos Jesús Vivar on 9/3/14.
+//  Copyright (c) 2014 DevSpark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
